48522f50cc244ad760b9d609ef27f936222a328d
Benjamin L <Benjamin.Labonne.03@outlook.fr>
2024-11-08 17:27:36 +0100

    print refactoring to eliminate useless concatenation, the main problem is the print speed of the terminal now

--- w_cli_control.py
+++ w_cli_control.py
@@ -18,14 +18,14 @@ def first_writeImageGR(image):
    s=""
    for i in range(len(image)) :
         for j in range(len(image[0])) :
-            s+= "\033[%d;%d;H" % (i+1, j+1) + "\033[48;5;%dm" % (232+image[i][j]) + " "
+            s+= "\033[%d;%d;H\033[48;5;%dm " % (i+1, j+1,232+image[i][j])
    print(s)
 def writeImageGR(image,previousFrame):
    s=""
    for i in range(len(image)) :
         for j in range(len(image[0])) :
             if(image[i][j] != previousFrame[i][j]):
-               s+= "\033[%d;%d;H" % (i+1, j+1) + "\033[48;5;%dm" % (232+image[i][j]) + " "
+               s+= "\033[%d;%d;H\033[48;5;%dm " % (i+1, j+1,232+image[i][j])
    print(s)
 
 
@@ -35,13 +35,13 @@ def first_writeImageFC(image):
    for i in range(len(image)) :
         for j in range(len(image[0])) :
             b, g, r = image[i][j];
-            s+= "\033[%d;%d;H" % (i+1, j+1) + "\033[48;2;%d;%d;%dm" % (r,g,b) + " "
+            s += "\033[%d;%d;H\033[48;2;%d;%d;%dm " % (i+1, j+1, r,g,b)
    print(s)    
 def writeImageFC(image,previousFrame):
    s=""
    for i in range(len(image)) :
         for j in range(len(image[0])) :
             if((image[i][j] != previousFrame[i][j]).any()):
-               b, g, r = image[i][j];
-               s+= "\033[%d;%d;H" % (i+1, j+1) + "\033[48;2;%d;%d;%dm" % (r,g,b) + " "
+               b, g, r = image[i][j]
+               s+= "\033[%d;%d;H\033[48;2;%d;%d;%dm " % (i+1, j+1, r,g,b)
    print(s)
